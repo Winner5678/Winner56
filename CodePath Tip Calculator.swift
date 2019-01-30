de0bbdd1a946a8556dcf7fb9fd892fbc11e9f8fa
Winner5678 <45828677+Winner5678@users.noreply.github.com>
2019-01-30 16:25:02 -0500

    Add files via upload

---
+++ CodePath Tip Calculator.swift
@@ -0,0 +1,13 @@
+//
+//  CodePath Tip Calculator.swift
+//  Tippy
+//
+//  Created by d.tang2 on 1/30/19.
+//  Copyright © 2019 codepath. All rights reserved.
+//
+
+import UIKit
+
+class CodePath_Tip_Calculator: NSObject {
+
+}
